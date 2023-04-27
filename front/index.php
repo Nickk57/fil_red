8ad72d2578b72ce56ea35dc385086331aa277f4c
Nickk57 <114159229+Nickk57@users.noreply.github.com>
2023-04-27 22:32:22 +0200

    tout

--- front/index.php
+++ front/index.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+    require_once('view/bar_nav.php');
+    require_once('../back/model/model.php');
+
+    if(isset($_GET['page']) && $_GET['page'] !=null) {
+        $page = strval($_GET['page']);
+
+        if($page == 1){
+            include_once('view/accueil.php');
+        }
+        elseif($page == 2){
+            include_once('view/cafe_grain.php');
+        }
+        elseif($page == 2){
+            include_once('view/cafe_moulu.php');
+        }
+        elseif($page == 3){
+            include_once('view/cafe_capsule.php');
+        }
+        elseif($page == 4){
+            include_once('view/contact.php');
+        }
+        elseif($page == 5){
+            include_once('view/compte.php');
+        }
+        elseif($page == 6){
+            include_once('view/panier.php');
+        }
+        else {
+            include_once("index.php");
+        }
+    }
---
+++ front/view/bar_nav.php
@@ -0,0 +1,10 @@
+<nav class="nav">
+    <a class="nav-link" aria-current="page" href="index.php"><img src="" alt="logo" width="50"></a>
+    <a class="nav-link" href="index.php?page=">accueil</a>
+    <a class="nav-link" href="index.php?page=">café en grain</a>
+    <a class="nav-link" href="index.php?page=">café moulu</a>
+    <a class="nav-link" href="index.php?page=">café en capsule</a>
+    <a class="nav-link" href="index.php?page=">contact</a>
+    <a class="nav-link" href="index.php?page="><img src="" alt="compte"></a>
+    <a class="nav-link" href="index.php?page="><img src="" alt="panier"></a>
+</nav>
